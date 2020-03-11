70f33b75d06418e895947424ec64126d477387ac
Básthy Soma <basthy.soma@gmail.com>
2020-03-11 18:40:25 +0100

    Navigation between login and register page todo: codebehind

--- temalab/LoginScreen.xaml.cs
+++ temalab/LoginScreen.xaml.cs
@@ -13,22 +13,28 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace temalab
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LoginScreen : Page
     {
-        public MainPage()
+        public LoginScreen()
         {
             this.InitializeComponent();
         }
-        private void StartClick(object sender, RoutedEventArgs e)
+
+        private void ToRegisterPage(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(KnownUsers));
+            //TODO: Hajrá Barna
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            //Codebehind login
         }
     }
 }
