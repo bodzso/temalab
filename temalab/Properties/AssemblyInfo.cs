9d38a14e0babc200cd3002c0a67db4f5295f73b2
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2020-03-14 14:46:53 +0100

    Add descriptions

--- temalab/Properties/AssemblyInfo.cs
+++ temalab/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("temalab")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Költségvetés kezelő rendszer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("temalab")]
@@ -27,3 +28,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
