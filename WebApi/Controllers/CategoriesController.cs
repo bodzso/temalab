323a70ac445812ad9b1509e58b0d6e0f566b1b9d
bence <karcagi.bence@gmail.com>
2020-03-29 20:25:32 +0200

    remove unused usings

--- WebApi/Controllers/CategoriesController.cs
+++ WebApi/Controllers/CategoriesController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApi.Entities;
--- WebApi/Entities/Transaction.cs
+++ WebApi/Entities/Transaction.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace WebApi.Entities
 {
