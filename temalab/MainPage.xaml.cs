2f447e921c65a0678505bdc5aa3c389139ad76f0
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2020-04-02 18:37:28 +0200

    Implement page navigation

--- temalab/MainPage.xaml.cs
+++ temalab/MainPage.xaml.cs
@@ -1,16 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,29 +24,133 @@ namespace temalab
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+        {
+            ("overview", typeof(UserMainPage)),
+            ("revenues", typeof(RevenuesPage)),
+            ("expenses", typeof(ExpensesPage)),
+            ("transactions", typeof(TransactionsPage)),
+        };
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void SwitchToOverview(object sender, TappedRoutedEventArgs e)
+        private void contentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
         {
-            contentFrame.Navigate(typeof(UserMainPage));
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
-        private void SwitchToRevenue(object sender, TappedRoutedEventArgs e)
+        private void navView_Loaded(object sender, RoutedEventArgs e)
         {
-            contentFrame.Navigate(typeof(RevenuesPage));
-        }
-        
-        private void SwitchToExpense(object sender, TappedRoutedEventArgs e)
-        {
-            contentFrame.Navigate(typeof(ExpensesPage));
+            navView.SelectedItem = navView.MenuItems[0];
+
+            navView_Navigate("overview", new EntranceNavigationTransitionInfo());
+
+            var goBack = new KeyboardAccelerator { Key = VirtualKey.GoBack };
+            goBack.Invoked += BackInvoked;
+            this.KeyboardAccelerators.Add(goBack);
+
+            var altLeft = new KeyboardAccelerator
+            {
+                Key = VirtualKey.Left,
+                Modifiers = VirtualKeyModifiers.Menu
+            };
+            altLeft.Invoked += BackInvoked;
+            this.KeyboardAccelerators.Add(altLeft);
         }
 
-        private void SwitchToTransactionsList(object sender, TappedRoutedEventArgs e)
+        private void navView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
-            contentFrame.Navigate(typeof(TransactionsPage));
+            if (args.IsSettingsInvoked == true)
+            {
+                navView_Navigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                navView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void navView_Navigate(string navItemTag, NavigationTransitionInfo transitionInfo)
+        {
+            Type _page = null;
+            if (navItemTag == "settings")
+            {
+                _page = typeof(SettingsPage);
+            }
+            else if(navItemTag == "overview")
+            {
+                _page = ((App)Application.Current).currentuserIsNew ? typeof(FreshUserMainPage) : typeof(UserMainPage);
+            }
+            else
+            {
+                var item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+                _page = item.Page;
+            }
+
+            var preNavPageType = contentFrame.CurrentSourcePageType;
+
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                contentFrame.Navigate(_page, null, transitionInfo);
+            }
+        }
+
+        private void navView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            On_BackRequested();
+        }
+
+        private void BackInvoked(KeyboardAccelerator sender,
+                         KeyboardAcceleratorInvokedEventArgs args)
+        {
+            On_BackRequested();
+            args.Handled = true;
+        }
+
+        private bool On_BackRequested()
+        {
+            if (!contentFrame.CanGoBack)
+                return false;
+
+            if (navView.IsPaneOpen &&
+                (navView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 navView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            contentFrame.GoBack();
+            return true;
+        }
+
+        private void contentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            navView.IsBackEnabled = contentFrame.CanGoBack;
+
+            if (contentFrame.SourcePageType == typeof(SettingsPage))
+            {
+                // SettingsItem is not part of navView.MenuItems, and doesn't have a Tag.
+                navView.SelectedItem = (NavigationViewItem)navView.SettingsItem;
+                navView.Header = "Settings";
+            }
+            else if(contentFrame.SourcePageType == typeof(FreshUserMainPage))
+            {
+                navView.Header = "Overview";
+                navView.SelectedItem = navView.MenuItems[0];
+            }
+            else if (contentFrame.SourcePageType != null)
+            {
+                var item = _pages.FirstOrDefault(p => p.Page == e.SourcePageType);
+
+                navView.SelectedItem = navView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Tag));
+
+                navView.Header =
+                    ((NavigationViewItem)navView.SelectedItem)?.Content?.ToString();
+            }
         }
     }
 }
