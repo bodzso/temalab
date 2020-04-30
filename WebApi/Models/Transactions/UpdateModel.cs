fadedcb145fa34aa745cebc0ecb9348bcb003506
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2020-04-30 18:54:59 +0200

    Add transaction update model

---
+++ WebApi/Models/Transactions/UpdateModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Transactions
+{
+    public class UpdateModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? CategoryId { get; set; }
+    }
+}
