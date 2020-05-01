793ec0c92dd5254a80ef9c98c4e418a96798a006
Zsombor Bodnár <bodnar.zsombor@gmail.com>
2020-05-01 10:14:06 +0200

    Remove required from name proprety

--- WebApi/Models/Transactions/UpdateModel.cs
+++ WebApi/Models/Transactions/UpdateModel.cs
@@ -6,7 +6,6 @@ namespace WebApi.Models.Transactions
     public class UpdateModel
     {
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public int? CategoryId { get; set; }
